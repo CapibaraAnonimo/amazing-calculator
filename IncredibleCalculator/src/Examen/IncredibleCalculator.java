3c94261d72b10753b032b4056899a55bbb6b8ca5
Adrián Arnaiz Cano <adrianarnaizcano@gmail.com>
2021-12-02 09:07:46 +0100

    Solucionado cosas de caracteres raros

--- IncredibleCalculator/src/Examen/IncredibleCalculator.java
+++ IncredibleCalculator/src/Examen/IncredibleCalculator.java
@@ -8,46 +8,46 @@ public class IncredibleCalculator {
         int aux1 = 0;
         int aux2 = 0;
 
-        System.out.println("Selecciona la operaci�n que prefieras realizar: ");
+        System.out.println("Selecciona la operación que prefieras realizar: ");
         System.out.println("1. Sumar");
         System.out.println("2. Restar");
         System.out.println("3. Multiplicar");
-        System.out.println("4. Dividir (divisi�n entera)");
-        System.out.println("5. Dividir (divisi�n con decimales)");
+        System.out.println("4. Dividir (división entera)");
+        System.out.println("5. Dividir (división con decimales)");
 
         int opcion = leerEnteroEntrada();
 
 
         switch (opcion) {
-            case 1: //Sumar. Debes leer dos numeros enteros de entrada e imprimir su suma
+            case 1: //Sumar. Debes leer dos números enteros de entrada e imprimir su suma
                 System.out.println("Introduce el primer termino");
                 aux1 = leerEnteroEntrada();
                 System.out.println("Introduce el primer termino");
                 aux2 = leerEnteroEntrada();
                 System.out.printf("%d", suma(aux1, aux2));
                 break;
-            case 2: // Restar. Debes leer dos n�meros enteros de entrada e imprimir su resta
+            case 2: // Restar. Debes leer dos números enteros de entrada e imprimir su resta
                 System.out.println("Introduce el primer termino");
                 aux1 = leerEnteroEntrada();
                 System.out.println("Introduce el primer termino");
                 aux2 = leerEnteroEntrada();
                 System.out.printf("%d", resta(aux1, aux2));
                 break;
-            case 3: // Multiplicar. Debes leer dos n�meros enteros de entrada e imprimir su producto.
+            case 3: // Multiplicar. Debes leer dos números enteros de entrada e imprimir su producto.
                 System.out.println("Introduce el primer termino");
                 aux1 = leerEnteroEntrada();
                 System.out.println("Introduce el segundo termino");
                 aux2 = leerEnteroEntrada();
                 multiplicacion(aux1, aux2);
                 break;
-            case 4: // Multiplicar. Debes leer dos n�meros enteros de entrada e imprimir su divisi�n entera.
+            case 4: // Multiplicar. Debes leer dos números enteros de entrada e imprimir su división entera.
                 System.out.println("Introduce el primer termino");
                 aux1 = leerEnteroEntrada();
                 System.out.println("Introduce el segundo termino");
                 aux2 = leerEnteroEntrada();
                 divisionEntera(aux1, aux2);
                 break;
-            case 5: // Multiplicar. Debes leer dos n�meros enteros de entrada e imprimir su divisi�n con decimales.
+            case 5: // Multiplicar. Debes leer dos números enteros de entrada e imprimir su división con decimales.
                 System.out.println("Introduce el primer termino");
                 aux1 = leerEnteroEntrada();
                 System.out.println("Introduce el segundo termino");
@@ -86,7 +86,7 @@ public class IncredibleCalculator {
             sc.close();
             return result;
         } catch (NumberFormatException ex) {
-            System.err.println("Lo que has introducido no es un n�mero entero!");
+            System.err.println("Lo que has introducido no es un número entero!");
             System.exit(1);
             return -1;
         }
